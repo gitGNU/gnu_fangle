73f7b3eba153756b7e1cde34cde3959c49e6488a
Sam Liddicott <sam@liddicott.com>
2014-07-18 16:35:03 +0100

    Improve context menus

--- init-fangle.scm
+++ init-fangle.scm
@@ -6,8 +6,32 @@
    )
   (menu-extend texmacs-extra-menu
     (=> "Fangle"
+      ("New chunk" (make-nf-chunk))
+      ("New chunk reference" (make-nf-ref))
       ("List of Fangle chunks" (make-aux* "the-index*" "nf-chunk" "Code Index"))
+      ("Extract All" (fangle-extract-all))
     ))
+  (kbd-map
+    ("M-c" (make-nf-chunk))
+    ("M-r" (make-nf-ref))
+  )
+)
+
+; (make (string->symbol "nf-chunk"))
+(define (make-nf-chunk)
+  (insert '(nf-chunk "" "" "" (tuple "")))
+)
+
+(define (make-nf-ref)
+  (make (string->symbol "nf-ref"))
+)
+
+(define (fangle-extract-all)
+  (let* ((name (string-append (url->system (current-buffer)) ".txt"))
+         (tmp-name (string-append name "~")) )
+    (export-buffer-main (current-buffer) tmp-name "verbatim" (list :overwrite))
+    (system-2 "fangle-refresh" name tmp-name)
+  )
 )
 
 (plugin-configure fangle
